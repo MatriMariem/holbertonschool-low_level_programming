e26700ab7bcccb2025d4ca6ecf786853e7c5f2dd
MatriMariem <meriemmatri1994@gmail.com>
2019-09-24 01:27:01 -0700

    correct

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,12 @@
 
 /**
 *_abs - gives the absolute value of an int
-*@int: is an integer
+*@n: is an integer
 *Return: abs value
 */
 
-int _abs(int)
+int _abs(int n)
 {
-int n;
 if (n < 0)
 {
 n = n * (-1);
