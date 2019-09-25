2f6dbf93ef8b8ded34f789e15be3e3fd1f3a4164
MatriMariem <meriemmatri1994@gmail.com>
2019-09-25 02:23:59 -0700

    fibonacci 50 numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,10 +12,10 @@ int i;
 Fi[0] = 1;
 Fi[1] = 2;
 printf("%ld, %ld ,", Fi[0], Fi[1]);
-for (i = 2; i <= 50; i++)
+for (i = 2; i <= 49; i++)
 {
 Fi[i] = Fi[i-1] + Fi[i-2];
-if (i != 50)
+if (i != 49)
 {
 printf("%ld, ", Fi[i]);
 }
