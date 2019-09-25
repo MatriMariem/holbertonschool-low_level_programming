5e1e1310ffc84e06c64b8c05d230fb33990a3da2
MatriMariem <meriemmatri1994@gmail.com>
2019-09-25 14:10:36 -0700

    done!

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,36 @@
  */
 int main(void)
 {
-long x, y, z, i;
+long x, y, z, i, xd, xr, yd, yr, p1, p2;
 x = 1;
 y = 2;
 printf("%ld, %ld, ", x, y);
-for (i = 3; i <= 97; i++)
+for (i = 3; i <= 45; i++)
 {
 z = x + y;
 printf("%ld, ", z);
 x = y;
 y = z;
 }
-printf("%ld\n", z);
+xd = x / 1000000000;
+xr = x % 1000000000;
+yd = y / 1000000000;
+yr = y % 1000000000;
+for (i = 46; i <= 97; i++)
+{
+p1 = xr + yr;
+p2 = xd + yd;
+if (p1 > 1000000000)
+{
+p1 = p1 % 1000000000;
+p2++;
+}
+printf("%ld%ld, ", p2, p1);
+xd = yd;
+xr = yr;
+yd = p2;
+yr = p1;
+}
+printf("%ld%ld\n", p2, p1);
 return (0);
 }
