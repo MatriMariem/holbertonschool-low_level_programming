1bd8f95d639cf57555c01f1d27d5453d477a2aec
MatriMariem <meriemmatri1994@gmail.com>
2019-09-25 02:38:54 -0700

    correct fibonacci 50 numberz

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,17 +11,16 @@ long Fi[50];
 int i;
 Fi[0] = 1;
 Fi[1] = 2;
-printf("%ld, %ld ,", Fi[0], Fi[1]);
 for (i = 2; i <= 49; i++)
 {
 Fi[i] = Fi[i - 1] + Fi[i - 2];
-if (i != 49)
-{
-printf("%ld, ", Fi[i]);
 }
-else
+for (i = 0; i <= 49; i++)
 {
 printf("%ld", Fi[i]);
+if (i != 49)
+{
+printf(", ");
 }
 }
 printf("\n");
