1077561251416993e4a0a17b87e1a7194d1aca2c
MatriMariem <meriemmatri1994@gmail.com>
2019-09-25 02:18:43 -0700

    fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+long int Fi[50];
+int i;
+Fi[0] = 1;
+Fi[1] = 2;
+printf("%ld, %ld ,", Fi[0], Fi[1]);
+for (i = 2; i <= 50; i++)
+{
+Fi[i] = Fi[i-1] + Fi[i-2];
+if (i != 50)
+{
+printf("%ld, ", Fi[i]);
+}
+else
+{
+printf("%ld", Fi[i]);
+}
+}
+printf("\n");
+return (0);
+}
