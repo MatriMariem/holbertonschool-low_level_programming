e17e08507ef864767e5f6d0edfb955694d93b09f
MatriMariem <meriemmatri1994@gmail.com>
2019-09-27 13:04:55 -0700

    success

--- 0x04-more_functions_nested_loops/origincode.c
+++ 0x04-more_functions_nested_loops/origincode.c
@@ -6,31 +6,34 @@
 */
 void print_number(int n)
 {
-int i, num, div, o;
-o = n % 10;
-n = n / 10;
-if (o < 0)
+int i, j, num, div;
+if ((n < 0) && (n > -2147483648))
 {
-n = -n;
-o = -o;
 _putchar('-');
 }
 num = n;
-div = 1;
-if (num > 0)
-{
+i = 1;
 while ((num / 10) != 0)
 {
 num = num / 10;
+i++;
+}
+div = 1;
+for (j = 1; j <= i - 1; j++)
+{
 div = div * 10;
 }
 while (div >= 1)
 {
-i = n / div;
-_putchar(i + '0');
+if (n < 0)
+{
+_putchar(((n / div)*(-1)) + '0');
+}
+else
+{
+_putchar((n / div)+'0');
+}
 n = n % div;
 div = div / 10;
 }
-}
-_putchar(o + '0');
 }
