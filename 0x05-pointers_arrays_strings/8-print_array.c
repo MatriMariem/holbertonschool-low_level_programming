ed5eff94d3362f4e937bf088b4f92143c4cb904d
MatriMariem <meriemmatri1994@gmail.com>
2019-10-01 06:14:48 -0700

    fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,10 +10,14 @@ void print_array(int *a, int n)
 {
 int i;
 i = 0;
-while (i < (n - 1))
+while (i < n)
 {
-printf("%d, ", *(a + i));
+printf("%d", *(a + i));
+if (i < (n - 1))
+{
+printf(", ");
+}
 i++;
 }
-printf("%d\n", *(a + i));
+printf("\n");
 }
