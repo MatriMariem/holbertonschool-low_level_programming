cde9424e137149266b0599bfbbdc37b671e61d2e
MatriMariem <meriemmatri1994@gmail.com>
2019-10-01 08:21:05 -0700

    advanced1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@ int _atoi(char *s)
 int i, integer, count = 0;
 i = 0;
 integer = 0;
-while (!((s[i] >= '0') && (s[i] <= '9')))
+while (!((s[i] >= '0') && (s[i] <= '9')) && (s[i] != '\0'))
 {
 if (s[i] == '-')
 {
