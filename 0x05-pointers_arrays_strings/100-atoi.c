90025f8fcfb5246d15c0b7870ec391087f6a7813
MatriMariem <meriemmatri1994@gmail.com>
2019-10-01 08:02:52 -0700

    advanced1

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* _atoi - converts a string into an integer
+*@s: pointer to a string
+*Return: the integer
+*/
+int _atoi(char *s)
+{
+int i, integer, count = 0;
+i = 0;
+integer = 0;
+while (!((s[i] >= '0') && (s[i] <= '9')))
+{
+if (s[i] == '-')
+{
+count++;
+}
+i++;
+}
+while ((s[i] >= '0') && (s[i] <= '9'))
+{
+integer = (integer * 10) + s[i];
+i++;
+}
+if (((count % 2) != 0) && (integer != 0))
+{
+integer = integer * (-1);
+}
+return (integer);
+}
