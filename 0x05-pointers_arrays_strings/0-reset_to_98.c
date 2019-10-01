d665e27ea03f44e44d693a62e12a6bcf24368590
MatriMariem <meriemmatri1994@gmail.com>
2019-10-01 01:11:34 -0700

    let's start

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+* reset_to_98 - it resets the int n to 98
+*@n: pointer to an int variable
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+learning how to use pointers, arrays and strings in C programming language
