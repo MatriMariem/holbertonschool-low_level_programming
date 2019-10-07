8e5248eb58ba2a0f6c63e03810b7157e29519d78
MatriMariem <meriemmatri1994@gmail.com>
2019-10-07 05:34:17 -0700

    correct till task 3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,14 +7,17 @@
 */
 char *_strchr(char *s, char c)
 {
-int i = 0;
-while (s[i] != '\0')
+int i = 0, l = 0;
+while (s[l])
+{
+l++;
+}
+for (i = 0; i <= l; i++)
 {
 if (s[i] == c)
 {
-return (&s[i]);
+return (&(s[i]));
 }
-i++;
 }
 return (0);
 }
