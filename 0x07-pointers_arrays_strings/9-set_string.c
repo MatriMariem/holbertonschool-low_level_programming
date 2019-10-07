c33be3571488b391b2082a39bdaf654a91e5526d
MatriMariem <meriemmatri1994@gmail.com>
2019-10-07 13:08:13 -0700

    5 still not fixed

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -2,7 +2,7 @@
 /**
 *set_string - sets the value of a pointer to a char
 *@s: pointer to a pointer
-*@to: pointer to a string 
+*@to: pointer to a string
 */
 void set_string(char **s, char *to)
 {
