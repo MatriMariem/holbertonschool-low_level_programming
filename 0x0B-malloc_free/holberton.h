03df87ff88be9208ff5f7b05b5aa20d9edc0892e
MatriMariem <meriemmatri1994@gmail.com>
2019-10-16 16:01:29 -0700

    ll

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -8,4 +8,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+void char_free_grid(char **grid, int height);
+int num_words(char *s);
 #endif
