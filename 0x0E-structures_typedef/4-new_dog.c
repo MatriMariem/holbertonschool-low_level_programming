363d8a7a373f741b8956a75a74a3faa39afdc0b6
MatriMariem <meriemmatri1994@gmail.com>
2019-10-21 08:40:06 -0700

    try 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+*new_dog - creates a new dog
+*@name: pointer to a string
+*@age: float
+*@owner: pointer to a string
+* Return: pointer to a variable
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *d;
+d = malloc(sizeof(dog_t));
+if (d == '\0')
+{
+return;
+}
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
