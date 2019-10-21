00144cb9d07a4db8ffe8b5357ad5f3020e417782
MatriMariem <meriemmatri1994@gmail.com>
2019-10-21 14:27:15 -0700

    let's try

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,8 +16,23 @@ if (d == '\0')
 {
 return (0);
 }
-d->name = name;
+d->name = malloc(sizeof(char) * (_strlen(name) + 1));
 d->age = age;
-d->owner = owner;
+d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 return (d);
+}
+#include "dog.h"
+/**
+* _strlen - it gives the length of a string
+*@s: pointer to a string
+* Return: the length of string
+*/
+int _strlen(char *s)
+{
+int i = 0;
+while (*(s + i) != '\0')
+{
+i++;
+}
+return (i);
 }
