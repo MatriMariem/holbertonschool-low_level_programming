201f1ced880194c85e5c108d073c8a99db080766
MatriMariem <meriemmatri1994@gmail.com>
2019-10-21 05:50:06 -0700

    start

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Each dog has the following
+*@name: name of dog
+*@age: age of dog
+*@owner: owner name
+*/
+typedef struct dog
+{
+char *name;
+float *age;
+char *owner;
+};
+#endif
