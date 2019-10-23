54765466c17aeb09c7bdaeff677743f60db50ec9
MatriMariem <meriemmatri1994@gmail.com>
2019-10-23 04:26:56 -0700

    yupi

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+*print_name - prints a name
+*@name: pointer to string
+*@f: pointer to function
+*/
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
