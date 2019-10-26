b03cda502565600adc7affb2cc775ea43f5b5f97
MatriMariem <meriemmatri1994@gmail.com>
2019-10-26 07:16:46 -0700

    changing position of struct delaration

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,7 @@
 #ifndef VARIADIC
 #define VARIADIC
-#include <stdio.h>
 #include <stdarg.h>
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+
 /**
  * struct types - pointer to function with corresponding letter
  * @letter: type name
@@ -16,6 +12,18 @@ typedef struct types
 char *letter;
 void (*f)(va_list);
 } my_str;
+
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struct types - pointer to function with corresponding letter
+ * @letter: type name
+ * @f: print function
+ */
+
 void print_char(va_list ls);
 void print_integer(va_list ls);
 void print_float(va_list ls);
