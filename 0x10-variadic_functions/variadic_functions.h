a05127518b1651d911eb474325f7e07c894a9c6e
MatriMariem <meriemmatri1994@gmail.com>
2019-10-26 07:46:46 -0700

    deleted functions from .h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -18,8 +18,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list ls);
-void print_integer(va_list ls);
-void print_float(va_list ls);
-void print_string(va_list ls);
 #endif
