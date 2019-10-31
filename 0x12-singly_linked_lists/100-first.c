d8f7a3e81c78e187be1e6bfcc2cc18fc8acf553e
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-10-31 15:32:49 -0700

    adv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+*Before_main - function executed before main function
+*/
+void Before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
