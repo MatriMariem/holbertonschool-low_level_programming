cae85b082af0582686b6013189f1d38005629196
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-11-07 14:09:59 -0800

    first advanced task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+*get_endianness - checks the endianness
+*Return: 0 if big endian, and 1 if little endian
+*/
+int get_endianness(void)
+{
+unsigned int n = 1;
+
+char *c = (char *)&n;
+
+if (*c)
+return (1);
+else
+return (0);
+
+
+}
