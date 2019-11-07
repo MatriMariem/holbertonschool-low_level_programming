7e270884cb5c2063291afd642aed0a51cef507dc
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-11-07 11:35:25 -0800

    task 2 check6

--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -16,5 +16,13 @@ int main(void)
     printf("%d\n", n);
     n = get_bit(1024, 0);
     printf("%d\n", n);
+ n = get_bit(1024, 11);
+    printf("%d\n", n);
+ n = get_bit(1, 0);
+    printf("%d\n", n);
+ n = get_bit(0, 1);
+    printf("%d\n", n);
+ n = get_bit(98, 0);
+    printf("%d\n", n);
     return (0);
 }
