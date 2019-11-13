2601e730716c0aaaa770d0ed3ede311bc97044bb
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-11-13 04:03:53 -0800

    task 3 betty + loop

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,35 @@
 #include "holberton.h"
 
 /**
+*copying - copy the content of file1 into file2
+*@fd1: file descriptor of file 1
+*@fd2: file descriptor of file 2
+*@av1: name of file 1
+*@av2: name of file 2
+*/
+void copying(int fd1, int fd2, char *av1, char *av2)
+{
+int rl, rw;
+char mybuff[1024];
+while ((rl = read(fd1, &mybuff, 1024)) != 0)
+{
+rw = write(fd2, &mybuff, rl);
+	if (rw < 0)
+	{
+	dprintf(2, "Error: Can't write to %s\n", av2);
+	exit(99);
+	}
+}
+
+if (rl < 0)
+{
+dprintf(2, "Error: Can't read from file %s\n", av1);
+exit(98);
+}
+}
+
+
+/**
  * main - copies the content of a file to another file
  * @ac: number of arguments
  *@av: arguments
@@ -10,8 +39,7 @@
  */
 int main(int ac, char **av)
 {
-int rl, rw, fd1, fd2;
-char mybuff[1024];
+int fd1, fd2;
 if (ac != 3)
 {
 dprintf(2, "Usage: cp file_from file_to\n");
@@ -23,26 +51,15 @@ if (fd1 == -1)
 dprintf(2, "Error: Can't read from file %s\n", av[1]);
 exit(98);
 }
-fd2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+fd2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT,
+S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (fd2 < 0)
 {
 dprintf(2, "Error: Can't write to %s\n", av[2]);
+close(fd1);
 exit(99);
 }
-while ((rl = read(fd1, &mybuff, 1024)) != 0)
-{
-rw = write(fd2, &mybuff, rl);
-if (rw < 0)
-{
-dprintf(2, "Error: Can't write to %s\n", av[2]);
-exit(99);
-}
-}
-if (rl < 0)
-{
-dprintf(2, "Error: Can't read from file %s\n", av[1]);
-exit(98);
-}
+copying(fd1, fd2, av[1], av[2]);
 if (close(fd1) < 0)
 {
 dprintf(2, "Error: Can't close fd %d\n", fd1);
@@ -55,3 +72,4 @@ exit(100);
 }
 return (0);
 }
+
