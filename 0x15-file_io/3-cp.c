6643e9efbf4383502cf05653b316e96d4b1a6f4c
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2019-11-13 04:51:12 -0800

    change 2 to STDERR_FILENO

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,16 +16,16 @@ char mybuff[1024];
 while ((rl = read(fd1, mybuff, 1024)) > 0)
 {
 rw = write(fd2, mybuff, rl);
-	if (rw != rl)
+	if (rw == -1)
 	{
-	dprintf(2, "Error: Can't write to %s\n", av2);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av2);
 	exit(99);
 	}
 }
 
 if (rl == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", av1);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av1);
 exit(98);
 }
 }
@@ -42,33 +42,33 @@ int main(int ac, char **av)
 int fd1, fd2, c1, c2;
 if (ac != 3)
 {
-dprintf(2, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 fd1 = open(av[1], O_RDONLY);
 if (fd1 == -1)
 {
-dprintf(2, "Error: Can't read from file %s\n", av[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 exit(98);
 }
 fd2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT,
 S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 if (fd2 == -1)
 {
-dprintf(2, "Error: Can't write to %s\n", av[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 exit(99);
 }
 copying(fd1, fd2, av[1], av[2]);
 c1 = close(fd1);
 if (c1 == -1)
 {
-dprintf(2, "Error: Can't close fd %d\n", fd1);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
 exit(100);
 }
 c2 = close(fd2);
 if (c2 == -1)
 {
-dprintf(2, "Error: Can't close fd %d\n", fd2);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 exit(100);
 }
 return (0);
