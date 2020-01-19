b2db8cc60a34d63cc31daf772edd843c4280fc9d
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-01-19 14:14:47 +0100

    check 5

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
-	if (size == 0)
-		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
+	if (size == 0)
+		return (table);
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!table->array)
 		return (NULL);
